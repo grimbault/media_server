5da3a71c9baa639eb517a9aba93f3d4e28394e99
grimbault <guillaume@rimbault.org>
2020-09-27 09:45:07 +0200

    Fix languages

--- main.py
+++ main.py
@@ -5,15 +5,41 @@ import xml.etree.ElementTree as ET
 import subprocess
 import configparser
 
-# Country codes (for subtitles download)
-languages=['EN', 'FR']
+# Country codes
+# 0 - Unknow
+# 1 - English
+# 2 - French
+# 3 - spanish
+# 4 - German
+# 5 - Italian
+# 6 - Danish
+# 7 - Dutch
+# 8 - Japanese
+# 9 - Icelandic
+# 10 - "Chinese
+# 11 - "Russian
+# 12 - "Polish
+# 13 - "Vietnamese
+# 14 - "Swedish
+# 15 - "Norwegian
+# 16 - "Finnish
+# 17 - "Turkish
+# 18 - "Portuguese
+# 19 - "Flemish
+# 20 - "Greek
+# 21 - "Korean
+# 22 - "Hungarian
+# 23 - "Hebrew
+# 24 - "Lithuanian
+# 25 - "Czech
+languages=[('En', 1), ('FR', 2)]
 
 credentials = configparser.ConfigParser()
 credentials.read('credentials.ini')
 
-IP = subprocess.check_output(["hostname -I | cut -d' ' -f1"], shell=True)
+IP = (subprocess.check_output(["hostname -I | cut -d' ' -f1"], shell=True)
         .split()[0]
-        .decode('ascii')
+        .decode('ascii'))
 
 os.system('''sed -i 's/${OWNIP}/%s/g' config/bazarr/config/config.ini'''%IP)
 
@@ -28,21 +54,21 @@ with open('config/jackett/Jackett/ServerConfig.json') as f:
 jackettAPI=data['APIKey']
 
 radarrAPI=''
-root = ET.fromstring('config/radarr/config.xml')
+root = ET.parse('config/radarr/config.xml').getroot()
 for child in root:
     if (child.tag=='ApiKey'):
-        os.system('''sed -i 's/${sonarr_key}/%s/g' config/bazarr/config/config.ini'''
+        os.system('''sed -i 's/${radarr_key}/%s/g' config/bazarr/config/config.ini'''
                                 %child.text)
 
 sonarrAPI=''
-root = ET.fromstring('config/sonarr/config.xml')
+root = ET.parse('config/sonarr/config.xml').getroot()
 for child in root:
     if (child.tag=='ApiKey'):
         os.system('''sed -i 's/${sonarr_key}/%s/g' config/bazarr/config/config.ini'''
                                 %child.text)
 
 for tool in ['sonarr', 'radarr']:
-    conn = sqlite3.connect('config/%s/nzbdrone.db'%i)
+    conn = sqlite3.connect('config/%s/nzbdrone.db'%tool)
 
     c = conn.cursor()
 
@@ -67,7 +93,7 @@ for tool in ['sonarr', 'radarr']:
             "olderMoviePriority": 0,  
             "addPaused": false,  
             "useSsl": false
-        }','DelugeSettings')''' % IP, tool)
+        }','DelugeSettings')''' % (IP, tool))
 
 
     # Additionals columns for radarr
@@ -80,70 +106,65 @@ for tool in ['sonarr', 'radarr']:
 
     for index, language in enumerate(languages):
         
+        # 0  -> Unknown
+        # 1  -> SDTV
+        # 2  -> DVD
+        # 3  -> WEBDL-1080p
+        # 4  -> HDTV-720p
+        # 5  -> WEBDL-720p
+        # 6  -> Bluray-720p
+        # 7  -> Bluray-1080p
+        # 8  -> WEBDL-480p
+        # 9  -> HDTV-1080p
+        # 10 -> Raw-HD
+        # 11 -> HDTV-480
+        # 12 -> WEBRip-480
+        # 13 -> Bluray-480
+        # 14 -> WEBRip-720
+        # 15 -> WEBRip-1080
+        # 16 -> HDTV-2160p
+        # 17 -> WEBRip-2160
+        # 18 -> WEBDL-2160p
+        # 19 -> Bluray-2160p
+        # 20 -> Bluray-1080p Remux
+        # 21 -> Bluray-2160p Remux
+
         c.execute('''INSERT INTO "Profiles" VALUES (%s,'Any %s',3,'[  
-            {    "quality": 0,    "allowed": false  },  
-            {    "quality": 1,    "allowed": false  },  
-            {    "quality": 8,    "allowed": false  },  
-            {    "quality": 2,    "allowed": false  },  
-            {    "quality": 4,    "allowed": true  },  
-            {    "quality": 9,    "allowed": true  },  
-            {    "quality": 10,    "allowed": false  },  
-            {    "quality": 5,    "allowed": true  },  
-            {    "quality": 6,    "allowed": true  },  
-            {    "quality": 3,    "allowed": true  },  
-            {    "quality": 7,    "allowed": true  },  
-            {    "quality": 16,    "allowed": false  },  
-            {    "quality": 18,    "allowed": false  }, 
-            {    "quality": 19,    "allowed": false  }
-            ]',%s %s)'''%((index-1)*4)+index, language, index, columns))
-        c.execute('''INSERT INTO "Profiles" VALUES (%s,'720p %s',5,'[  
-            {    "quality": 0,    "allowed": false  },  
-            {    "quality": 1,    "allowed": false  },  
-            {    "quality": 8,    "allowed": false  },  
-            {    "quality": 2,    "allowed": false  },  
-            {    "quality": 4,    "allowed": true  },  
-            {    "quality": 9,    "allowed": false  },  
-            {    "quality": 10,    "allowed": false  },  
-            {    "quality": 5,    "allowed": true  },  
-            {    "quality": 6,    "allowed": true  },  
-            {    "quality": 3,    "allowed": false  },  
-            {    "quality": 7,    "allowed": false  },  
-            {    "quality": 16,    "allowed": false  },  
-            {    "quality": 18,    "allowed": false  },  
-            {    "quality": 19,    "allowed": false  }
-            ]',%s %s)'''%((index-1)*4)+index+1, language, index, columns))
+            { "quality": 0,  "allowed": false },  
+            { "quality": 1,  "allowed": false },  
+            { "quality": 2,  "allowed": false },  
+            { "quality": 3,  "allowed": true },  
+            { "quality": 4,  "allowed": true },  
+            { "quality": 5,  "allowed": true },  
+            { "quality": 6,  "allowed": false },  
+            { "quality": 7,  "allowed": false },  
+            { "quality": 8,  "allowed": false },  
+            { "quality": 9,  "allowed": true },  
+            { "quality": 10, "allowed": false },  
+            { "quality": 16, "allowed": false },  
+            { "quality": 18, "allowed": false }, 
+            { "quality": 19, "allowed": false }
+            ]',%s %s)'''%(((index)*4)+index+1, language[0], language[1], columns))
+        # 720p
+        c.execute('''INSERT INTO "Profiles" VALUES (%s,'720p %s',5,'[ 
+            { "quality": 4,  "allowed": true },  
+            { "quality": 5,  "allowed": true },  
+            { "quality": 6,  "allowed": false }
+            ]',%s %s)'''%(((index)*4)+index+2, language[0], language[1], columns))
+        # 1080p
         c.execute('''INSERT INTO "Profiles" VALUES (%s,'1080p %s',3,'[  
-            {    "quality": 0,    "allowed": false  },  
-            {    "quality": 1,    "allowed": false  },  
-            {    "quality": 8,    "allowed": false  },  
-            {    "quality": 2,    "allowed": false  },  
-            {    "quality": 4,    "allowed": false  }, 
-            {    "quality": 9,    "allowed": true  },  
-            {    "quality": 10,    "allowed": false  },  
-            {    "quality": 5,    "allowed": false  },  
-            {    "quality": 6,    "allowed": false  },  
-            {    "quality": 3,    "allowed": true  },  
-            {    "quality": 7,    "allowed": true  },  
-            {    "quality": 16,    "allowed": false  },  
-            {    "quality": 18,    "allowed": false  },  
-            {    "quality": 19,    "allowed": false  }
-            ]',%s %s)'''%((index-1)*4)+index+2, language, index, columns))
-        c.execute('''INSERT INTO "Profiles" VALUES (%s,'Ultra-HD %s',19,'[  
-            {    "quality": 0,    "allowed": false  }, 
-            {    "quality": 1,    "allowed": false  },  
-            {    "quality": 8,    "allowed": false  },  
-            {    "quality": 2,    "allowed": false  },  
-            {    "quality": 4,    "allowed": false  },  
-            {    "quality": 9,    "allowed": false  },  
-            {    "quality": 10,    "allowed": false  },  
-            {    "quality": 5,    "allowed": false  },  
-            {    "quality": 6,    "allowed": false  },  
-            {    "quality": 3,    "allowed": false  },  
-            {    "quality": 7,    "allowed": false  },  
-            {    "quality": 16,    "allowed": true  },  
-            {    "quality": 18,    "allowed": true  },  
-            {    "quality": 19,    "allowed": true  }
-            ]',%s %s)'''%((index-1)*4)+index+3, language, index, columns))
+            { "quality": 9,  "allowed": true },
+            { "quality": 3,  "allowed": true },  
+            { "quality": 7,  "allowed": false },
+            { "quality": 20,  "allowed": false }
+            ]',%s %s)'''%(((index)*4)+index+3, language[0], language[1], columns))
+        # 2160p
+        c.execute('''INSERT INTO "Profiles" VALUES (%s,'Ultra-HD %s',19,'[ 
+            { "quality": 16, "allowed": true },  
+            { "quality": 18, "allowed": true },  
+            { "quality": 19, "allowed": true },
+            { "quality": 21, "allowed": true }
+            ]',%s %s)'''%(((index)*4)+index+4, language[0], language[1], columns))
 
     # Specificities from radarr & sonarr
     columns=''
